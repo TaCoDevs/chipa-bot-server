a6f6816e33dae9778c620a91b6ee70f27468c19b
Max Alejandro <max.flores.ar@gmail.com>
2022-10-16 18:59:17 -0500

    more methods

---
+++ src/routes/modules/playlist.ts
@@ -0,0 +1,25 @@
+/*
+ 
+crear rutas para interactuar con la base de datos
+
+*/
+
+import express from "express";
+
+const router = express.Router();
+
+// get playlist de usuario
+
+router.get("/:id", (req, res) => {
+    //
+});
+
+router.post("/add", (req, res) => {
+    //
+});
+
+router.delete("/remove/:id", (req, res) => {
+    //
+});
+
+export default router;
---
+++ src/services/playlist.service.ts
@@ -0,0 +1,29 @@
+/*
+
+conectar a la base de datos
+
+iniciar base de datos en libs/base de datos
+
+-agregar una cancion a la playlist de un usuario
+
+-remover cancion en la playlist de un usuario
+
+-obtener la playlist de un usuario
+
+*/
+
+class PlaylistService {
+    addToPlaylist() {
+        //
+    }
+
+    removeToPlaylist() {
+        //
+    }
+
+    getUserPlaylist() {
+        //
+    }
+}
+
+export default PlaylistService;
